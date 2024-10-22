ed755163d94e19b92cca65c005228fd9177c34fb
Hannes Matuschek <hannes.matuschek@uni-potsdam.de>
2024-10-22 15:34:03 +0200

    Added menu to image and pattern view.

--- src/elementpatterneditor.cc
+++ src/elementpatterneditor.cc
@@ -18,6 +18,9 @@ ElementPatternEditor::ElementPatternEditor(QWidget *parent)
                            ui->actionElementEditorSplitUnknownField,
                            ui->actionElementEditorMarkAsUnknown });
 
+  ui->actionElementEditorSplitUnknownField->setIcon(QIcon::fromTheme("edit-split-unknown"));
+  ui->actionElementEditorEditPattern->setIcon(QIcon::fromTheme("edit"));
+
   connect(ui->editor, &ElementPatternView::selectionChanged,
           this, &ElementPatternEditor::onSelectionChanged);
   connect(ui->editor, &ElementPatternView::doubleClicked,
