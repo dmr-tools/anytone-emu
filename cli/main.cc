bbf6e0ff90cf010956883a6f27a326141a9ee5c5
Hannes Matuschek <hannes.matuschek@uni-potsdam.de>
2026-01-08 11:08:07 +0100

    Updated commandline parser.

--- cli/main.cc
+++ cli/main.cc
@@ -33,25 +33,33 @@ main(int argc, char *argv[])
   parser.addVersionOption();
 
   parser.addOption({"loglevel", "Sets the log-level. Must be one of 'debug', 'info', 'warning', "
-                    "'error' or 'fatal'. Default: 'warning'.", "loglevel", "warning"});
+                    "'error' or 'fatal'. Default: 'info'.", "loglevel", "info"});
   parser.addOption({"output", "Specifies the sprintf-like file pattern to save received codeplugs to. "
-                    "For every received codeplug, a counter gets increased. E.g., 'codeplug_%02d.hex' "
+                    "For every codeplug received, a counter gets increased. E.g., 'codeplug_%02d.hex' "
                     "will produce the file-sequence 'codeplug_00.hex', 'codeplug_01.hex', ...",
                     "output", "codeplug_%04d.hex"});
   parser.addOption({"dump", "Just dumps received codeplugs. Either to stdout or to a file, "
                     "if --output ist set."});
   parser.addOption({"diff", "Shows diffs between received codeplugs. The mode specifies, which "
-                    "differences are taken and shown. If 'first' (default) is set, the difference "
-                    "is alway between the first and last received codeplugs. If 'previous' is set, "
-                    "the diffs are taken between the last two received codeplugs.", "mode", "first"});
-  parser.addOption({"device", "Specifies the device to emulate. This must be one of 'd868uv', "
-                    "'d868uve', 'd878uv', 'd878uv2', 'd578uv', 'd578uv2', 'dmr6x2uv', 'dmr6x2uv2', "
-                    "'djmd5' or 'djmd5x'.", "device"});
-  parser.addOption({"interface", "Specifies the interface to the CPS. This can either be 'pty' or "
-                    "the name of a serial interface. If 'pty' is set, the emulator will generate a "
-                    "new pseudo terminal. This can then be used to emulate a COM port under wine. "
-                    "Under windows, a serial port of a virtual null-modem must be choosen. The "
-                    "second port of that null-modem is then selected in the CPS.", "interface", "pty"});
+                    "differences are taken and shown. If 'first' is set, the difference "
+                    "is always between the first and last received codeplugs. If 'previous' (default) is set, "
+                    "the diffs are taken between the last two received codeplugs.", "mode", "previous"});
+  parser.addOption({"firmware", "Specifies the firmware version to emulate. "
+                    "This usually has no effect on the emulation. If no firmware is specified, "
+                    "the latest found is used. If set to '?' all firmwares defined for the model are "
+                    "shown.", "firmware", "latest"});
+
+  parser.addPositionalArgument("catalog", "Specifies the catalog file to use. "
+                               "See http://github.com/dmr-tools/codeplugs/.");
+  parser.addPositionalArgument("device", "Specifies the device to emulate, e.g. 'd868uv', "
+                               "'d868uve', 'd878uv', 'd878uv2', 'd578uv', 'd578uv2', 'dmr6x2uv', 'dmr6x2uv2', "
+                               "'djmd5' or 'djmd5x'. For a full list of the devices of the catalog, "
+                               "omit this argument.", "[device");
+  parser.addPositionalArgument("interface", "Specifies the interface to the CPS. This can either be 'pty' or "
+    "the name of a serial interface. If 'pty' is set, the emulator will generate a "
+    "new pseudo terminal. This can then be used to emulate a COM port under wine. "
+    "Under windows, a serial port of a virtual null-modem must be choosen. The "
+    "second port of that null-modem is then selected in the CPS.", "[interface]]");
 
   parser.process(app);
 
@@ -66,14 +74,14 @@ main(int argc, char *argv[])
   else if ("fatal" == parser.value("loglevel"))
     Logger::get().addHandler(new StreamLogHandler(err, LogMessage::FATAL, true));
 
-  if ( (parser.isSet("dump") && parser.isSet("diff")) ||
-       (!parser.isSet("dump") && !parser.isSet("diff")) )
+  // If no catalog is specified -> print help and exit
+  if (0 == parser.positionalArguments().size())
     parser.showHelp(-1);
 
   QTextStream stream(stdout);
   ModelCatalog catalog;
   ModelDefinitionParser modelParser(&catalog);
-  QFile catalogFile(":/codeplug/catalog.xml");
+  QFile catalogFile(parser.positionalArguments().at(0));
   if (! catalogFile.open(QIODevice::ReadOnly)) {
     logError() << "Cannot parse catalog file '" << catalogFile.fileName()
                << "': " << catalogFile.errorString() << ".";
@@ -86,27 +94,61 @@ main(int argc, char *argv[])
     return -1;
   }
 
-  if (! catalog.hasModel(parser.value("device"))) {
-    logError() << "Cannot find model '" << parser.value("device")
-               << "' unknown device.";
+  if ((2 > parser.positionalArguments().size()) ||
+      (!catalog.hasModel(parser.positionalArguments().at(1)))) {
+    if (2 <= parser.positionalArguments().size())
+      logError() << "Cannot find model '" << parser.positionalArguments().at(1)
+                 << "' unknown device.";
+    QStringList devices;
+    for (auto dev: catalog)
+      devices.append(QString("%1 (%2)").arg(dev->name()).arg(dev->id()));
+    logInfo() << "The following devices are specified in '" << catalogFile.fileName()
+              << "': \n  * " << devices.join("\n  * ") << ".";
     return -1;
   }
-  ModelDefinition *modelDef = catalog.model(parser.value("device"));
-  ModelFirmwareDefinition *modelFirmwareDef = modelDef->latestFirmware();
-  /// @todo Use optional argument to determine firmware version.
+
+  ModelDefinition *modelDef = catalog.model(parser.positionalArguments().at(1));
+  if (nullptr == modelDef) {
+    logError() << "Mode '" << parser.positionalArguments().at(1)
+               << "' not found in catalog " << catalogFile.fileName() << ".";
+    return -1;
+  }
+
+  ModelFirmwareDefinition *modelFirmwareDef = nullptr;
+  if ((! parser.isSet("firmware")) || ("latest" == parser.value("firmware")))
+    modelFirmwareDef = modelDef->latestFirmware();
+  else
+    modelFirmwareDef = modelDef->firmware(parser.value("firmware"));
+  if (nullptr == modelFirmwareDef) {
+    if ((!parser.isSet("firmware")) || ("?" != parser.value("firmware")))
+      logError() << "Cannot find firmware '" << parser.value("firmware")
+                 << "' for device " << modelDef->name()
+                 << " in " << catalogFile.fileName() << ".";
+    QStringList fwVersions;
+    for (auto fw: *modelDef)
+      fwVersions.append(fw->name());
+    logInfo() << "Possible firmware version for " << modelDef->name()
+              << ": " << fwVersions.join(", ") << ".";
+    return -1;
+  }
 
   QString portFile = QDir::home().absoluteFilePath(".local/share/anytone-emu/anytoneport");
   QIODevice *interface = nullptr;
-  if ("pty" == parser.value("interface")) {
+  if (3 > parser.positionalArguments().size()) {
+    logError() << "No interface specified.";
+    parser.showHelp(-1);
+  }
+  auto interfaceName = parser.positionalArguments().at(2);
+  if ("pty" == interfaceName) {
     interface = new PseudoTerminal(portFile);
   } else {
-    QSerialPortInfo portInfo(parser.value("interface"));
+    QSerialPortInfo portInfo(interfaceName);
     if (portInfo.isNull()) {
       QStringList names;
       foreach (QSerialPortInfo port, QSerialPortInfo::availablePorts())
         names.append(port.portName());
-      QString nameList = names.size() ? names.join(", ") : "[no ports found]";
-      logError() << "Cannot open serial port '" << parser.value("interface")
+      QString nameList = (! names.empty()) ? names.join(", ") : "[no ports found]";
+      logError() << "Cannot open serial port '" << interfaceName
                  << "', use one of the known ports: " << nameList << ".";
       return -1;
     }
@@ -151,7 +193,7 @@ main(int argc, char *argv[])
       else
         logInfo() << "No differences found.";
     });
-  } else if ("previous" == parser.value("diff")) {
+  } else if ((!parser.isSet("diff")) || ("previous" == parser.value("diff"))) {
     QObject::connect(imageHandler, &ImageCollector::imageReceived, [&stream, imageHandler] {
       if ((nullptr == imageHandler->previous()) || (nullptr == imageHandler->last()))
         return;
