52bf309f4c65269a8d0198da5565b78d79976e62
Hannes Matuschek <hmatuschek@gmail.com>
2026-03-12 12:37:10 +0100

    Implemented user dictionary.

--- src/spellcheckedtextedit.cc
+++ src/spellcheckedtextedit.cc
@@ -1,10 +1,57 @@
 #include "spellcheckedtextedit.hh"
 #include "spellcheckhighlighter.hh"
+#include "spellchecker.hh"
+#include "application.hh"
+#include <QMenu>
 
 
 SpellCheckedTextEdit::SpellCheckedTextEdit(QWidget *parent)
-  : QTextEdit(parent)
+  : QTextEdit(parent), _highlighter(new SpellCheckHighlighter(this))
 {
-  auto highlighter = new SpellCheckHighlighter(this);
-  highlighter->setDocument(this->document());
+  _highlighter->setDocument(this->document());
+}
+
+
+void
+SpellCheckedTextEdit::contextMenuEvent(QContextMenuEvent *event) {
+  auto menu = createStandardContextMenu(event->pos());
+  if (! event->pos().isNull()) {
+    auto checker = qobject_cast<Application *>(QApplication::instance())->spellChecker();
+
+    QTextCursor tc = cursorForPosition(event->pos());
+    tc.select(QTextCursor::WordUnderCursor);
+    auto word = tc.selectedText();
+    if (! checker->check(word)) {
+      menu->addSeparator();
+      auto suggestions = checker->suggest(word);
+      if (! suggestions.isEmpty()) {
+        QMenu *subMenu = menu->addMenu(tr("Replace %1 with ...").arg(word));
+        for (auto suggestion: suggestions) {
+          auto action = subMenu->addAction(suggestion);
+          int start = tc.selectionStart(), end = tc.selectionEnd();
+          connect(action, &QAction::triggered, [this, suggestion, start, end] () {
+            this->fixSpelling(start, end, suggestion);
+          });
+        }
+      }
+      auto addAction = menu->addAction(tr("Add word"));
+      connect(addAction, &QAction::triggered, [this, word, &tc]() {
+        qobject_cast<Application*>(QApplication::instance())->spellChecker()->addWord(word);
+        this->_highlighter->rehighlight();
+      });
+    }
+  }
+  menu->exec(event->globalPos());
+  delete menu;
+}
+
+
+void
+SpellCheckedTextEdit::fixSpelling(int from, int to, const QString &replace) {
+  auto cursor = textCursor();
+  cursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor, from);
+  cursor.select(QTextCursor::WordUnderCursor);
+  cursor.removeSelectedText();
+  cursor.insertText(replace);
+  _highlighter->rehighlight();
 }
--- src/spellcheckedtextedit.hh
+++ src/spellcheckedtextedit.hh
@@ -3,12 +3,24 @@
 
 #include <QTextEdit>
 
+class SpellCheckHighlighter;
+
+
 class SpellCheckedTextEdit : public QTextEdit
 {
   Q_OBJECT
 
 public:
   explicit SpellCheckedTextEdit(QWidget *parent=nullptr);
+
+protected:
+  void contextMenuEvent(QContextMenuEvent *event) override;
+
+protected slots:
+  void fixSpelling(int from, int to, const QString &replace);
+
+private:
+  SpellCheckHighlighter *_highlighter;
 };
 
 #endif // SPELLCHECKEDTEXTEDIT_HH
